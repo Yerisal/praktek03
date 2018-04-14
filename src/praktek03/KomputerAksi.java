2638f84bb22cee2c6314beb95553c3a77493b55c
Yerisal Christofel Lodo <rizallodo@gmail.com>
2018-04-14 22:36:20 +0800

    Instance, buat attribut dan panggil method

--- src/praktek03/KomputerAksi.java
+++ src/praktek03/KomputerAksi.java
@@ -2,6 +2,11 @@ package praktek03;
 
 public class KomputerAksi {
     public static void main(String[] args) {
+        Komputer a = new Komputer();
+        a.merk = "ACER";
+        a.processor = "intel core i3";
+        a.memory = "2 GB";
         
+        a.cetakInfo();
     }
 }
