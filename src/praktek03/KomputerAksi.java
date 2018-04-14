630bee0ab0786d1d480651acd2d269d5f396640b
Yerisal Christofel Lodo <rizallodo@gmail.com>
2018-04-14 22:46:13 +0800

    Menggunakan method set

--- src/praktek03/KomputerAksi.java
+++ src/praktek03/KomputerAksi.java
@@ -3,9 +3,10 @@ package praktek03;
 public class KomputerAksi {
     public static void main(String[] args) {
         Komputer a = new Komputer();
-        a.merk = "ACER";
-        a.processor = "intel core i3";
-        a.memory = "2 GB";
+
+        a.setMerk("ASUS");
+        a.setProcessor("intel core i5");
+        a.setMemory("4 GB");
         
         a.cetakInfo();
     }
