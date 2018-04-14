279664c03e6d4c83828a7c927590147ea8fa2cd7
Yerisal Christofel Lodo <rizallodo@gmail.com>
2018-04-14 22:29:50 +0800

    Buat method cetakInfo()

--- src/praktek03/Komputer.java
+++ src/praktek03/Komputer.java
@@ -4,4 +4,10 @@ public class Komputer {
     String merk;
     String processor;
     String memory;
+    
+    void cetakInfo(){
+        System.out.println("Merk        \t: "+merk+"\n"+
+                "Processor  \t: "+processor+"\n"+
+                "Memory     \t: "+memory);
+    }
 }
